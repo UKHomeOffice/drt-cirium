1d602177c10ac0a65947175c7f3e8008488664f2
Rich Birch <ringo@albumsnaps.com>
2022-04-22 09:51:49 +0100

    DRTII-991 Add logging when there is est chox before est runway

--- src/main/scala/uk/gov/homeoffice/cirium/services/entities/CiriumEntities.scala
+++ src/main/scala/uk/gov/homeoffice/cirium/services/entities/CiriumEntities.scala
@@ -2,9 +2,9 @@ package uk.gov.homeoffice.cirium.services.entities
 
 import akka.http.scaladsl.model.Uri
 import org.joda.time.DateTime
+import org.slf4j.{Logger, LoggerFactory}
 
-import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.duration._
+import scala.concurrent.duration.{FiniteDuration, _}
 import scala.language.postfixOps
 import scala.util.Try
 
@@ -13,15 +13,14 @@ import scala.util.Try
 case class CiriumInitialResponse(request: CiriumRequestMetaData, item: String) {
   def uri: Uri = Uri(item)
 }
+
 trait CiriumFlightStatusResponse
 
-case class CiriumFlightStatusResponseSuccess(
-  request: CiriumRequestMetaData,
-  flightStatuses: Option[List[CiriumFlightStatus]]) extends CiriumFlightStatusResponse
+case class CiriumFlightStatusResponseSuccess(request: CiriumRequestMetaData,
+                                             flightStatuses: Option[List[CiriumFlightStatus]]) extends CiriumFlightStatusResponse
 
-case class CiriumFlightStatusResponseFailure(
-  error: Throwable,
-  timestamp: Long = System.currentTimeMillis) extends CiriumFlightStatusResponse
+case class CiriumFlightStatusResponseFailure(error: Throwable,
+                                             timestamp: Long = System.currentTimeMillis) extends CiriumFlightStatusResponse
 
 case class CiriumItemResponse(request: CiriumRequestMetaData, item: String)
 
@@ -33,11 +32,10 @@ object CiriumItemListResponse {
 
 case class CiriumBatchSize(requested: String, interpreted: Int)
 
-case class CiriumRequestMetaData(
-  endpoint: String,
-  itemId: Option[CiriumItemId],
-  batchSize: Option[CiriumBatchSize],
-  url: String)
+case class CiriumRequestMetaData(endpoint: String,
+                                 itemId: Option[CiriumItemId],
+                                 batchSize: Option[CiriumBatchSize],
+                                 url: String)
 
 case class CiriumItemId(requested: String, interpreted: String)
 
@@ -52,70 +50,69 @@ object CiriumDate {
 
 case class CiriumCodeshare(fsCode: String, flightNumber: String, relationship: String)
 
-case class CiriumDelays(
-  departureGateDelayMinutes: Option[Int],
-  departureRunwayDelayMinutes: Option[Int],
-  arrivalGateDelayMinutes: Option[Int],
-  arrivalRunwayDelayMinutes: Option[Int])
+case class CiriumDelays(departureGateDelayMinutes: Option[Int],
+                        departureRunwayDelayMinutes: Option[Int],
+                        arrivalGateDelayMinutes: Option[Int],
+                        arrivalRunwayDelayMinutes: Option[Int])
 
-case class CiriumFlightDurations(
-  scheduledBlockMinutes: Option[Int],
-  blockMinutes: Option[Int],
-  scheduledAirMinutes: Option[Int],
-  airMinutes: Option[Int],
-  scheduledTaxiOutMinutes: Option[Int],
-  taxiOutMinutes: Option[Int],
-  scheduledTaxiInMinutes: Option[Int],
-  taxiInMinutes: Option[Int])
+case class CiriumFlightDurations(scheduledBlockMinutes: Option[Int],
+                                 blockMinutes: Option[Int],
+                                 scheduledAirMinutes: Option[Int],
+                                 airMinutes: Option[Int],
+                                 scheduledTaxiOutMinutes: Option[Int],
+                                 taxiOutMinutes: Option[Int],
+                                 scheduledTaxiInMinutes: Option[Int],
+                                 taxiInMinutes: Option[Int])
 
 case class CiriumFlightStatusUpdate(updatedAt: CiriumDate, source: String)
 
-case class CiriumAirportResources(
-  departureTerminal: Option[String],
-  departureGate: Option[String],
-  arrivalTerminal: Option[String],
-  arrivalGate: Option[String],
-  baggage: Option[String])
+case class CiriumAirportResources(departureTerminal: Option[String],
+                                  departureGate: Option[String],
+                                  arrivalTerminal: Option[String],
+                                  arrivalGate: Option[String],
+                                  baggage: Option[String])
 
-case class CiriumOperationalTimes(
-  publishedDeparture: Option[CiriumDate],
-  scheduledGateDeparture: Option[CiriumDate],
-  estimatedGateDeparture: Option[CiriumDate],
-  actualGateDeparture: Option[CiriumDate],
-  flightPlanPlannedDeparture: Option[CiriumDate],
-  scheduledRunwayDeparture: Option[CiriumDate],
-  estimatedRunwayDeparture: Option[CiriumDate],
-  actualRunwayDeparture: Option[CiriumDate],
-  publishedArrival: Option[CiriumDate],
-  flightPlanPlannedArrival: Option[CiriumDate],
-  scheduledGateArrival: Option[CiriumDate],
-  estimatedGateArrival: Option[CiriumDate],
-  actualGateArrival: Option[CiriumDate],
-  scheduledRunwayArrival: Option[CiriumDate],
-  estimatedRunwayArrival: Option[CiriumDate],
-  actualRunwayArrival: Option[CiriumDate])
+case class CiriumOperationalTimes(publishedDeparture: Option[CiriumDate],
+                                  scheduledGateDeparture: Option[CiriumDate],
+                                  estimatedGateDeparture: Option[CiriumDate],
+                                  actualGateDeparture: Option[CiriumDate],
+                                  flightPlanPlannedDeparture: Option[CiriumDate],
+                                  scheduledRunwayDeparture: Option[CiriumDate],
+                                  estimatedRunwayDeparture: Option[CiriumDate],
+                                  actualRunwayDeparture: Option[CiriumDate],
+                                  publishedArrival: Option[CiriumDate],
+                                  flightPlanPlannedArrival: Option[CiriumDate],
+                                  scheduledGateArrival: Option[CiriumDate],
+                                  estimatedGateArrival: Option[CiriumDate],
+                                  actualGateArrival: Option[CiriumDate],
+                                  scheduledRunwayArrival: Option[CiriumDate],
+                                  estimatedRunwayArrival: Option[CiriumDate],
+                                  actualRunwayArrival: Option[CiriumDate])
 
-case class CiriumFlightStatus(
-  flightId: Int,
-  carrierFsCode: String,
-  operatingCarrierFsCode: String,
-  primaryCarrierFsCode: String,
-  flightNumber: String,
-  departureAirportFsCode: String,
-  arrivalAirportFsCode: String,
-  departureDate: CiriumDate,
-  arrivalDate: CiriumDate,
-  status: String,
-  operationalTimes: CiriumOperationalTimes,
-  delays: Option[CiriumDelays],
-  flightDurations: Option[CiriumFlightDurations],
-  codeshares: Seq[CiriumCodeshare],
-  airportResources: Option[CiriumAirportResources],
-  flightStatusUpdates: Seq[CiriumFlightStatusUpdate])
+case class CiriumFlightStatus(flightId: Int,
+                              carrierFsCode: String,
+                              operatingCarrierFsCode: String,
+                              primaryCarrierFsCode: String,
+                              flightNumber: String,
+                              departureAirportFsCode: String,
+                              arrivalAirportFsCode: String,
+                              departureDate: CiriumDate,
+                              arrivalDate: CiriumDate,
+                              status: String,
+                              operationalTimes: CiriumOperationalTimes,
+                              delays: Option[CiriumDelays],
+                              flightDurations: Option[CiriumFlightDurations],
+                              codeshares: Seq[CiriumCodeshare],
+                              airportResources: Option[CiriumAirportResources],
+                              flightStatusUpdates: Seq[CiriumFlightStatusUpdate]) {
+  val log: Logger = LoggerFactory.getLogger(getClass)
+  if (operationalTimes.estimatedGateArrival.nonEmpty && operationalTimes.estimatedRunwayArrival.isEmpty) {
+    log.info(s"$carrierFsCode${flightNumber} has est chox but no est runway time. TaxiInMinutes: ${flightDurations.flatMap(_.taxiInMinutes)}, TaxiOutMinutes: ${flightDurations.flatMap(_.taxiOutMinutes)}")
+  }
+}
 
 case class CiriumTrackableStatus(status: CiriumFlightStatus, messageUri: String, processedMillis: Long) {
-
-  def isInSync(threshold: FiniteDuration = 1 minute) = CiriumMessageFormat
+  def isInSync(threshold: FiniteDuration = 1 minute): Boolean = CiriumMessageFormat
     .dateFromUri(messageUri)
     .toOption
     .exists(issueDate => processedMillis - threshold.toMillis < issueDate.getMillis)
